60e2bca42fa7bb20e574abb4677ab3ab0be74a71
welvin21 <wewe2712@connect.hku.hk>
2020-04-10 00:26:11 +0800

    Fetch train labels from trainLabels.csv

--- server/model.py
+++ server/model.py
@@ -6,6 +6,7 @@ import sys
 import cv2
 import time
 import matplotlib
+import csv
 from subprocess import check_output
 
 from keras.models import Sequential
@@ -21,7 +22,9 @@ from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 
 # Declare constant variables
-TRAIN_DATASETS_PATH = os.path.join(os.path.sep, os.getcwd(), "datasets/train")
+CWD = os.getcwd()
+TRAIN_DATASETS_PATH = os.path.join(os.path.sep, CWD, "datasets/train")
+TRAIN_LABELS_PATH = os.path.join(os.path.sep, CWD, "datasets/trainLabels.csv")
 NUM_CLASSES = 5
 WIDTH = 128
 HEIGHT = 128
@@ -48,16 +51,22 @@ def convertClassToInt(label):
     return switcher.get(label, "Invalid class")
 
 
-def getTrainData(trainDir, numberOfTrainData = 1000):
+def getTrainData(trainDir, numberOfTrainData=1000):
     startTime = time.time()
-    if(numberOfTrainData > 35126):
-        print('ERROR: max number of train data exceeded')
+    if numberOfTrainData > 35126:
+        print("ERROR: max number of train data exceeded")
         return
-    
+
     global ImageNameDataHash
     images = os.listdir(trainDir)
-    print("Number of images found in {}: {} images.".format(trainDir, len(images)))
-    print("Fetching {} sample images from training datasets".format(numberOfTrainData))
+    print(
+        "INFO: Number of images found in {}: {} images.".format(trainDir, len(images))
+    )
+    print(
+        "INFO: Fetching {} sample images from training datasets".format(
+            numberOfTrainData
+        )
+    )
     for image in images:
         imageFullPath = os.path.join(os.path.sep, trainDir, image)
         loadedImage = load_img(imageFullPath)
@@ -82,10 +91,51 @@ def getTrainData(trainDir, numberOfTrainData = 1000):
         arr = np.array(arr, dtype="float")
         ImageNameDataHash[image.replace(".jpeg", "")] = np.array(arr)
 
-        if(len(ImageNameDataHash) == numberOfTrainData):
+        if len(ImageNameDataHash) == numberOfTrainData:
             break
-    print('Successfully loaded {} images within {} seconds'.format(numberOfTrainData, time.time() - startTime))
+    print(
+        "INFO: Successfully loaded {} images within {} seconds".format(
+            numberOfTrainData, time.time() - startTime
+        )
+    )
     return
 
 
+def getTrainLabels(fileLocation):
+    rawDF = pd.read_csv(fileLocation)
+    numOfRows, numOfColumns = rawDF.shape[0], rawDF.shape[1]
+    print(
+        "INFO: fetching train labels csv, found {} rows and {} columns of data".format(
+            numOfRows, numOfColumns
+        )
+    )
+
+    # Double check if left and right data of a particular patient are the same
+    rawDF['PatientID'] = ''
+    ImageLevelHash = {}
+    patientIDList = set()
+    for index, row in rawDF.iterrows():
+        key = row[0] + ''
+        patientID = row[0] + ''
+        patientID = patientID.replace('_right','')
+        patientID = patientID.replace('_left','')
+        rawDF.at[index, 'PatientID'] = patientID
+        patientIDList.add(patientID)
+        ImageLevelHash[key] = str(row[1])
+
+    inconsistentDataCount = 0
+    for patientID in patientIDList:
+        leftLevel = ImageLevelHash["{}_left".format(patientID)]
+        rightLevel = ImageLevelHash["{}_right".format(patientID)]
+
+        if(leftLevel != rightLevel):
+            inconsistentDataCount += 1
+            print("WARNING: patient {}'s data are not consistent. left -> {}. right -> {}".format(patientID, leftLevel, rightLevel))
+
+    print("INFO: number of inconsistent data: {}".format(inconsistentDataCount))
+    print("INFO: number of unique patients: {}".format(len(patientIDList)))
+    return rawDF
+
+
 getTrainData(TRAIN_DATASETS_PATH, 200)
+trainLabels = getTrainLabels(TRAIN_LABELS_PATH)
