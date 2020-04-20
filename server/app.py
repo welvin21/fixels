095221ee84040edee43693f6104faaa324dd7d9b
welvin21 <wewe2712@connect.hku.hk>
2020-04-21 01:17:07 +0800

    Add /predict endpoint

--- server/app.py
+++ server/app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+import os
+from flask import Flask, request
+from predict import predict
+from model import createModel
+
+WIDTH = 512
+HEIGHT = 512
+DEPTH = 3
+INPUT_SHAPE = (HEIGHT, WIDTH, DEPTH)
+NUM_OF_CLASSES = 2
+
+CWD = os.getcwd()
+PATH_TO_MODEL = os.path.join(os.path.sep, CWD, 'DR_model_weights_new.h5')
+model = createModel(INPUT_SHAPE, NUM_OF_CLASSES)
+model.load_weights(PATH_TO_MODEL)
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def main():
+def sample():
     return "ok"
+
+@app.route("/predict", methods=["POST"])
+def main():
+    req = request.get_json()
+    imageBase64 = req['imageBase64'] 
+
+    return str(predict(model, imageBase64))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, threaded=False)
--- server/predict.py
+++ server/predict.py
@@ -1,62 +1,37 @@
 import os
 import numpy as np
 import cv2
-from argparse import ArgumentParser
 from keras.models import load_model
 from keras.preprocessing.image import (
     img_to_array,
     load_img,
 )
 from model import createModel
-
-# fetch image data as paramater
-argParser = ArgumentParser()
-argParser.add_argument(
-    "-i", "--image", default='./', help="relative path to input image", type=str
-)
-argParser.add_argument(
-    "-m", "--model", required=True, help="relative path to model weights in .h5 format", type=str
-)
-args = vars(argParser.parse_args())
-
-# declare constant variables
-CWD = os.getcwd()
-PATH_TO_MODEL_WEIGHT = os.path.join(os.path.sep, CWD, args['model'])
-PATH_TO_INPUT_IMAGE = os.path.join(os.path.sep, CWD, args["image"])
+from base64 import b64decode
+from PIL import Image
+from io import BytesIO
 
 WIDTH = 512
 HEIGHT = 512
 DEPTH = 3
-inputShape = (HEIGHT, WIDTH, DEPTH)
-NUM_OF_CLASSES = 2
-# Initialize number of epochs to train for, initial learning rate and batch size
-BS = 32
-
 
 def convertIntToClass(i):
     switcher = {0: "No DR", 1: "DR"}
     return switcher.get(i, "Invalid class")
 
 
-def convertImage(imageFullPath):
-    loadedImage = load_img(imageFullPath)
-    arr = img_to_array(loadedImage)
+def resizeImageArray(arr):
     arr = cv2.resize(arr, (HEIGHT, WIDTH))
     arr = np.array(arr, dtype="float")
-
     arr = arr.reshape(-1, HEIGHT, WIDTH, DEPTH)
     return arr
 
-
-# load model
-model = createModel(inputShape, NUM_OF_CLASSES)
-model.load_weights(PATH_TO_MODEL_WEIGHT)
-
-
-def predict(pathToInputImage):
-    # convert image to np.array format
-    print("INFO: converting image {}".format(pathToInputImage))
-    arr = convertImage(pathToInputImage)
+def predict(model, imageBase64):
+    imageBase64Decoded = b64decode(imageBase64)
+    image = Image.open(BytesIO(imageBase64Decoded))
+    arr = np.array(image)
+    arr = resizeImageArray(arr)
+    print(arr.shape)
 
     # predict the confidence values of each classes
     print("INFO: predicting input image\n")
@@ -73,7 +48,3 @@ def predict(pathToInputImage):
     predictedClass = convertIntToClass(predictedLabel[0])
     print("\nINFO: predicted class result: {}".format(predictedClass))
     return (predictionProbabilities, predictedLabel, predictedClass)
-
-
-if __name__ == "__main__":
-    predict(PATH_TO_INPUT_IMAGE)
