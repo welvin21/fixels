6a065fd810fa0cdb3600a36af37e82bc336a12e0
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-04-21 01:34:08 +0800

    Connect model to python server for prediction (#7)
    * Update requirements.txt
    
    * Add /predict endpoint
    
    * Format server response in json

--- server/app.py
+++ server/app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+import os
+import json
+from argparse import ArgumentParser
+from flask import Flask, request
+from predict import predict
+from model import createModel
+
+argsParser = ArgumentParser()
+argsParser.add_argument('-m', '--model', required=True, help='path to model weights in .h5 format')
+args = vars(argsParser.parse_args())
+
+WIDTH = 512
+HEIGHT = 512
+DEPTH = 3
+INPUT_SHAPE = (HEIGHT, WIDTH, DEPTH)
+NUM_OF_CLASSES = 2
+
+CWD = os.getcwd()
+PATH_TO_MODEL = os.path.join(os.path.sep, CWD, args['model'])
+model = createModel(INPUT_SHAPE, NUM_OF_CLASSES)
+model.load_weights(PATH_TO_MODEL)
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def main():
+def sample():
     return "ok"
+
+@app.route("/predict", methods=["POST"])
+def main():
+    req = request.get_json()
+    imageBase64 = req['imageBase64'] 
+
+    probabilities, predictedLabel, predictedClass = predict(model, imageBase64)
+    
+    prediction = {}
+    prediction['probabilities'] = dict(enumerate(probabilities))
+    prediction['label'] = predictedLabel
+    prediction['class'] = predictedClass
+    print(prediction)
+    return json.dumps(prediction)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, threaded=False)
