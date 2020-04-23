85172c35ecbd63e013a425a6574d5a23241b68a0
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-04-23 19:14:46 +0800

    Change server code to OOP (#13)
    * Change server implementation to OOP
    
    * Modify train.py

--- server/predict.py
+++ server/predict.py
@@ -6,7 +6,6 @@ from keras.preprocessing.image import (
     img_to_array,
     load_img,
 )
-from model import createModel
 from base64 import b64decode
 from PIL import Image
 from io import BytesIO
@@ -15,31 +14,33 @@ WIDTH = 512
 HEIGHT = 512
 DEPTH = 3
 
+class Predictor:
+    def __init__(self, model, imageBase64):
+        self.model = model
+        self.imageBase64 = imageBase64
 
-def convertIntToClass(i):
-    switcher = {0: "No DR", 1: "DR"}
-    return switcher.get(i, "Invalid class")
+    def convertIntToClass(self, i):
+        switcher = {0: "No DR", 1: "DR"}
+        return switcher.get(i, "Invalid class")
 
+    def resizeImageArray(self, arr):
+        arr = cv2.resize(arr, (HEIGHT, WIDTH))
+        arr = np.array(arr, dtype="float")
+        arr = arr.reshape(-1, HEIGHT, WIDTH, DEPTH)
+        return arr
 
-def resizeImageArray(arr):
-    arr = cv2.resize(arr, (HEIGHT, WIDTH))
-    arr = np.array(arr, dtype="float")
-    arr = arr.reshape(-1, HEIGHT, WIDTH, DEPTH)
-    return arr
+    def predict(self):
+        imageBase64Decoded = b64decode(self.imageBase64)
+        image = Image.open(BytesIO(imageBase64Decoded))
+        arr = np.array(image)
+        arr = self.resizeImageArray(arr)
 
+        # predict the confidence values of each classes
+        prediction = self.model.predict(arr)
 
-def predict(model, imageBase64):
-    imageBase64Decoded = b64decode(imageBase64)
-    image = Image.open(BytesIO(imageBase64Decoded))
-    arr = np.array(image)
-    arr = resizeImageArray(arr)
+        predictionProbabilities = prediction[0].tolist()
+        # generate the actual class predicted
+        predictedLabel = int(prediction.argmax(axis=-1)[0])
+        predictedClass = self.convertIntToClass(predictedLabel)
 
-    # predict the confidence values of each classes
-    prediction = model.predict(arr)
-
-    predictionProbabilities = prediction[0].tolist()
-    # generate the actual class predicted
-    predictedLabel = int(prediction.argmax(axis=-1)[0])
-    predictedClass = convertIntToClass(predictedLabel)
-
-    return (predictionProbabilities, predictedLabel, predictedClass)
+        return (predictionProbabilities, predictedLabel, predictedClass)
